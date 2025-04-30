35966f74e1ca502733cc64ec2bf8b23771166f7e
jungsunbeen <tjsqls041103@gmail.com>
2025-04-30 20:56:37 +0900

    feat: 5각형 진행중

--- src/view/YutBoardV2.java
+++ src/view/YutBoardV2.java
@@ -138,39 +138,78 @@ public class YutBoardV2 extends JPanel {
     private int[] convertToBoardIndex(int x, int y) {
         int boardX = 0, boardY = 0;
 
-        if (x == 491) {
-            boardY = (491 - y) / 56;
-            return new int[]{0, boardY};
-        } else if (y == 491) {
-            boardY = 20 - (491 - x) / 56;
-            return new int[]{0, boardY};
-        } else if (x == 209) {
-            boardY = 10 - (209 - y) / 56;
-            return new int[]{0, boardY};
-        } else if (y == 209) {
-            boardY = 5 + (491 - x) / 56;
-            return new int[]{0, boardY};
-        } else {
-            if (x == 256 && y == 256) return new int[]{2, 11};
-            if (x == 444 && y == 256) return new int[]{1, 6};
-            if (x == 303 && y == 303) return new int[]{2, 12};
-            if (x == 397 && y == 303) return new int[]{1, 7};
-            if (x == 350 && y == 350) return new int[]{1, 1};
-            if (x == 303 && y == 397) return new int[]{1, 9};
-            if (x == 397 && y == 397) return new int[]{2, 14};
-            if (x == 256 && y == 444) return new int[]{1, 10};
-            if (x == 444 && y == 444) return new int[]{2, 15};
+        if(numSides == 4){
+
+            if (x == 491) {
+                boardY = (491 - y) / 56;
+                return new int[]{0, boardY};
+            } else if (y == 491) {
+                boardY = 20 - (491 - x) / 56;
+                return new int[]{0, boardY};
+            } else if (x == 209) {
+                boardY = 10 - (209 - y) / 56;
+                return new int[]{0, boardY};
+            } else if (y == 209) {
+                boardY = 5 + (491 - x) / 56;
+                return new int[]{0, boardY};
+            } else {
+                if (x == 256 && y == 256) return new int[]{2, 11};
+                if (x == 444 && y == 256) return new int[]{1, 6};
+                if (x == 303 && y == 303) return new int[]{2, 12};
+                if (x == 397 && y == 303) return new int[]{1, 7};
+                if (x == 350 && y == 350) return new int[]{1, 1};
+                if (x == 303 && y == 397) return new int[]{1, 9};
+                if (x == 397 && y == 397) return new int[]{2, 14};
+                if (x == 256 && y == 444) return new int[]{1, 10};
+                if (x == 444 && y == 444) return new int[]{2, 15};
+            }
         }
+
+        if (numSides == 5){
+                if (x == 350 && y == 216) return new int[]{2, 11};
+                if (x == 350 && y == 283) return new int[]{2, 12};
+                if (x == 223 && y == 309) return new int[]{3, 16};
+                if (x == 286 && y == 329) return new int[]{3, 17};
+                if (x == 350 && y == 350) return new int[]{1, 8};  // 중심점
+                if (x == 413 && y == 329) return new int[]{1, 7};
+                if (x == 311 && y == 403) return new int[]{1, 9};
+                if (x == 389 && y == 403) return new int[]{2, 14};
+                if (x == 272 && y == 457) return new int[]{1, 10};
+                if (x == 428 && y == 457) return new int[]{2, 15};
+
+                // 가장자리 좌표
+                if (x == 350 && y == 150) return new int[]{0, 10};
+                if (x == 388 && y == 177) return new int[]{0, 9};
+                if (x == 426 && y == 205) return new int[]{0, 8};
+                if (x == 464 && y == 233) return new int[]{0, 7};
+                if (x == 502 && y == 261) return new int[]{0, 6};
+                if (x == 540 && y == 289) return new int[]{0, 5};
+                if (x == 476 && y == 309) return new int[]{1, 6};
+                if (x == 525 && y == 333) return new int[]{0, 4};
+                if (x == 510 && y == 377) return new int[]{0, 3};
+                if (x == 496 && y == 422) return new int[]{0, 2};
+                if (x == 481 && y == 466) return new int[]{0, 1};
+                if (x == 467 && y == 511) return new int[]{0, 0};
+                if (x == 420 && y == 514) return new int[]{0, 24};
+                if (x == 373 && y == 514) return new int[]{0, 23};
+                if (x == 326 && y == 513) return new int[]{0, 22};
+                if (x == 279 && y == 513) return new int[]{0, 21};
+                if (x == 233 && y == 513) return new int[]{0, 20};
+                if (x == 218 && y == 466) return new int[]{0, 19};
+                if (x == 203 && y == 422) return new int[]{0, 18};
+                if (x == 189 && y == 377) return new int[]{0, 17};
+                if (x == 174 && y == 333) return new int[]{0, 16};
+                if (x == 160 && y == 289) return new int[]{0, 15};
+                if (x == 198 && y == 261) return new int[]{0, 14};
+                if (x == 236 && y == 233) return new int[]{0, 13};
+                if (x == 274 && y == 205) return new int[]{0, 12};
+                if (x == 312 && y == 177) return new int[]{0, 11};
+            }
+
 
         return new int[]{boardX, boardY};
     }
 
-//    private void drawCircle(Graphics2D g2, int x, int y, int size) {
-//        g2.drawOval(x - size / 2, y - size / 2, size, size);
-//        Point boardIndex = convertToBoardIndex(x, y);
-//        g2.drawString("(" + x + ", " + y + ")", x - size / 2, y - size / 2 - 5);
-////        g2.drawString("[" + boardIndex.x + ", " + boardIndex.y + "]", x + size / 2, y + size / 2);
-//    }
     private void drawCircle(Graphics2D g2, int x, int y, int size) {
         g2.drawOval(x - size / 2, y - size / 2, size, size);
         int[] boardIndex = convertToBoardIndex(x, y);
