1230af7e068f54377c0663b50b599f55ffcce5e6
jungsunbeen <tjsqls041103@gmail.com>
2025-05-03 15:44:38 +0900

    add: Piece,Player.java

---
+++ src/model/Piece.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Piece {
+
+    private int[][] position;
+    private boolean isGrouped;
+    private boolean isFinished;
+
+    public Piece() {
+        this.position = new int[0][0]; // 초기값
+        this.isGrouped = false;
+        this.isFinished = false;
+    }
+
+    // Getters and Setters
+    public int[][] getPosition() {
+        return position;
+    }
+
+    public void setPosition(int[][] position) {
+        this.position = position;
+    }
+
+    public boolean isGrouped() {
+        return isGrouped;
+    }
+
+    public void setGrouped(boolean grouped) {
+        isGrouped = grouped;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+}
---
+++ src/model/Player.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.List;
+
+public class Player {
+    private int id;
+    private List<Piece> pieces;
+
+    public Player(int id, List<Piece> pieces) {
+        this.id = id;
+        this.pieces = pieces;
+    }
+
+    public Piece selectPiece(int index) {
+        return pieces.get(index);
+    }
+
+    public int checkRemainPieceNum() {
+        int count = 0;
+        for (Piece piece : pieces) {
+            if (!piece.isFinished()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Getter
+    public int getId() {
+        return id;
+    }
+
+    public List<Piece> getPieces() {
+        return pieces;
+    }
+}
