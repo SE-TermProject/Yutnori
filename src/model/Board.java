b52d8d64873658cffacb89a13ca3f33f1233481d
wldy4627 <wldy4627@gmail.com>
2025-05-07 00:48:14 +0900

    feat(#12): Board model 추가

---
+++ src/model/Board.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.List;
+
+public class Board {
+    private int numSides;
+    private int[][] cells;
+
+    public Board(int numSides) {
+        this.numSides = numSides;
+        this.cells = new int[5][35];
+    }
+
+    /* 위치가 비어있는지 확인 */
+    public boolean isOccupied(s) {
+
+    }
+
+    /* 이동 가능한 위치 계산 */
+    public List<int[]> findPossiblePos() {
+
+    }
+
+    /* 실제 말 이동 처리 */
+    public void movePieceTo() {
+
+    }
+
+    /* 상대 말 잡기 처리 */
+    public void catchPiece() {
+
+    }
+}
