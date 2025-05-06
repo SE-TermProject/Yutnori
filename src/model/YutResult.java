ecbd52ce7b8d6316ff6f1aae95e4114226beced4
wldy4627 <wldy4627@gmail.com>
2025-05-07 01:14:07 +0900

    feat(#12): YutResult.enum 추가

---
+++ src/model/YutResult.java
@@ -0,0 +1,25 @@
+package model;
+
+public enum YutResult {
+    BackDo(-1),
+    DO(1),
+    GAE(2),
+    GUL(3),
+    YUT(4),
+    MO(5);
+
+    private final int step;
+
+    YutResult(int step) {
+        this.step = step;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public boolean isBonusTurn() {
+        // 윷이나 모는 한 번 더 던질 수 있는 조건
+        return this == YUT || this == MO;
+    }
+}
