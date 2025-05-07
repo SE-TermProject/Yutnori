52e60f196306fb40ae69e62bac75c0221112a9fc
wldy4627 <wldy4627@gmail.com>
2025-05-07 15:33:07 +0900

    fix(#12): 말의 position 현재 위치 인덱스만 저장하도록 수정

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -2,22 +2,22 @@ package model;
 
 public class Piece {
 
-    private int[][] position;
+    private int[] position;
     private boolean isGrouped;
     private boolean isFinished;
 
     public Piece() {
-        this.position = new int[0][0]; // 초기값
+        this.position = new int[0]; // 초기값
         this.isGrouped = false;
         this.isFinished = false;
     }
 
     // Getters and Setters
-    public int[][] getPosition() {
+    public int[] getPosition() {
         return position;
     }
 
-    public void setPosition(int[][] position) {
+    public void setPosition(int[] position) {
         this.position = position;
     }
 
