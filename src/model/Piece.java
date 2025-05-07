e0255cbea304e7a13cffc87cf75ad29cc8930dc0
only4wxx <orchidbyw1@gmail.com>
2025-05-07 16:23:50 +0900

    feat(#4): 말이 이전에 이동했던 위치를 저장하는 필드 추가

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -1,15 +1,20 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Piece {
 
     private int[][] position;
     private boolean isGrouped;
     private boolean isFinished;
+    private List<int[]> prePositions; // 이전에 이동했던 위치 저장
 
     public Piece() {
         this.position = new int[0][0]; // 초기값
         this.isGrouped = false;
         this.isFinished = false;
+        this.prePositions = new ArrayList<>();
     }
 
     // Getters and Setters
@@ -36,4 +41,10 @@ public class Piece {
     public void setFinished(boolean finished) {
         isFinished = finished;
     }
+
+    public List<int[]> getPrePositions() { return prePositions; }
+
+    public void addPrePosition(int[] prePosition) { this.prePositions.add(prePosition); }
+
+    public void deletePrePosition(int prePosition) { this.prePositions.remove(prePosition); }
 }
