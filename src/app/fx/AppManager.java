5d5942387a45dd870f509267cc33c49a8527b8bc
wldy4627 <wldy4627@gmail.com>
2025-05-18 02:02:29 +0900

    feat(#80): YutBoard 레이아웃 초기 생성 (FX)

--- src/app/fx/AppManager.java
+++ src/app/fx/AppManager.java
@@ -1,6 +1,7 @@
 package app.fx;
 
 import controller.fx.YutController;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import view.fx.GameSetupView;
 import view.fx.YutBoard;
@@ -13,10 +14,15 @@ public class AppManager {
         new GameSetupView(primaryStage, this);
     }
 
-    private void startGame(int sides, int playerCount, int pieceCount) {
+    public void startGame(int sides, int playerCount, int pieceCount) {
         YutBoard board = new YutBoard();
         YutController yutController = new YutController(this, sides, playerCount, pieceCount, board);
         yutController.initializeGameUI();
+
+        Scene scene = new Scene(board, 1100, 700);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Yutnori");
+        primaryStage.show();
     }
 
     public void restartGame() {
