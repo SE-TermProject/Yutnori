5b807f106b91d108e54d01e8bb1ad62db8279cad
only4wxx <orchidbyw1@gmail.com>
2025-05-26 01:44:49 +0900

    refactor(#68): main 함수 필요 없는 내용 삭ㅈ

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,5 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        System.out.println("Hello world!");
-
-//        Board board = new Board(6);
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        while (true) {
-//            int row = Integer.parseInt(br.readLine());
-//            int col = Integer.parseInt(br.readLine());
-//            int step = Integer.parseInt(br.readLine());
-//
-//            board.findPossiblePos(new Stack<int[]>(), row, col, step);
-//        }
-
-        Game game = new Game(5, 2, 4);
-        game.findCurrentPossiblePos();
     }
 }
