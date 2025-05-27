ce27bb65e696983aad2c982e120d7337f4ec47de
only4wxx <orchidbyw1@gmail.com>
2025-05-27 17:52:23 +0900

    refactor(#86): import 줄이기

--- src/model/BoardPoint.java
+++ src/model/BoardPoint.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Objects;
-
 public class BoardPoint {
     private final int x;
     private final int y;
--- src/model/Piece.java
+++ src/model/Piece.java
@@ -1,8 +1,7 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Arrays;
 import java.util.Stack;
 
 public class Piece {
